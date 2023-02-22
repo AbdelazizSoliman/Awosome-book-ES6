19926bd201164e5e6ee68e6b71db6859f51098da
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-02-22 17:03:03 +0200

    Add modules and install luxon.js

--- index.js
+++ index.js
@@ -1,154 +1,76 @@
-/* eslint-disable */
-const data = document.querySelector("#data");
+import menu from './modules/menu.js';
+// eslint-disable-next-line import/no-unresolved
+import Book from './modules/BookStorage.js';
+import { DateTime } from './modules/luxon.js';
+import storebooks from './modules/storebooks.js';
 
-// Book class: Represent a book
-class Book {
-  constructor(title, authore) {
-    this.title = title;
-    this.authore = authore;
-  }
-}
-
-// UI Class: Handle UI Tasks
+menu();
 class UI {
   static displayBooks() {
-    const books = store.getBooks();
+    const books = storebooks.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
-    const list = document.querySelector("#data");
+    const list = document.querySelector('#data');
 
-    const div = document.createElement("div");
-    div.className = "data-container";
+    const div = document.createElement('div');
+    div.className = 'data-cont';
 
     div.innerHTML = `
-        <p>"${book.title}"</p>
-        <p>by</p>
-        <p>${book.authore}</p>
-        <div><button id="remove-btn" class='delete'>Remove</button></div>
-     `;
+          <p>"${book.title}"</p>
+          <p>by</p>
+          <p>${book.author}</p>
+          <div><button id="remove-btn" class='delete'>Remove</button></div>
+       `;
 
     list.appendChild(div);
   }
 
   static deleteBook(el) {
-    if (el.classList.contains("delete")) {
+    if (el.classList.contains('delete')) {
       el.parentElement.parentElement.remove();
     }
   }
 
   static clearField() {
-    document.querySelector("#title").value = "";
-    document.querySelector("#author").value = "";
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
   }
 }
 
-// store data in local storage
-class store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem("books") === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem("books"));
-    }
-    return books;
-  }
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-  static addBook(book) {
-    const books = store.getBooks();
-
-    books.push(book);
-
-    localStorage.setItem("books", JSON.stringify(books));
-  }
-
-  static removeBook(authore) {
-    const books = store.getBooks();
-    books.forEach((book, index) => {
-      if (book.authore === authore) {
-        books.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem("books", JSON.stringify(books));
-  }
-}
-
-//  Event for display books
-document.addEventListener("DOMContentLoaded", UI.displayBooks);
-
-// Add event listener to the add button
-document.querySelector("#form").addEventListener("submit", (e) => {
-  // Prevent actual submit
+// form submission to the add button
+document.querySelector('#form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = document.querySelector("#title").value;
-  const authore = document.querySelector("#author").value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-  // Instatiate book
-  const book = new Book(title, authore);
+  const book = new Book(title, author);
 
-  // Add book data to screen
+  // Add data to screen
   UI.addBookToList(book);
 
-  // Add book to store
-  store.addBook(book);
+  // Add book to storebooks
+  storebooks.addBook(book);
 
-  // clear fields
+  // clear all fields
   UI.clearField();
 });
 
-// Event to remove books
-document.querySelector("#data").addEventListener("click", (e) => {
-  // Remove book from UI
+// Removing book data
+const data = document.querySelector('#data');
+data.addEventListener('click', (e) => {
+  // Remove book from interface
   UI.deleteBook(e.target);
   // Remove book from local storage
-  store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  storebooks.removeBook(
+    e.target.parentElement.previousElementSibling.textContent,
+  );
 });
 
-// Navigation section start here
-document.getElementById("list").addEventListener("click", () => {
-  showList();
-});
-document.getElementById("add").addEventListener("click", () => {
-  showAdd();
-});
-document.getElementById("cont").addEventListener("click", () => {
-  showContact();
-});
-
-function defaultDisplay() {
-  document.getElementById("diplay-list").setAttribute("class", "non-show");
-  document.getElementById("input-data").setAttribute("class", "show");
-  document.getElementById("contact").setAttribute("class", "non-show");
-  document.getElementById("footer").setAttribute("class", "show");
-}
-defaultDisplay();
-
-function showList() {
-  document.getElementById("diplay-list").setAttribute("class", "show");
-  document.getElementById("input-data").setAttribute("class", "non-show");
-  document.getElementById("contact").setAttribute("class", "non-show");
-}
-
-function showAdd() {
-  document.getElementById("diplay-list").setAttribute("class", "non-show");
-  document.getElementById("input-data").setAttribute("class", "show");
-  document.getElementById("contact").setAttribute("class", "non-show");
-}
-
-function showContact() {
-  document.getElementById("diplay-list").setAttribute("class", "non-show");
-  document.getElementById("input-data").setAttribute("class", "non-show");
-  document.getElementById("contact").setAttribute("class", "show");
-}
-
-document.write(new Date().getFullYear());
-
-function ondate() {
-  document.querySelector(".date").innerHTML = Date();
-}
-
-ondate();
+// Luxon Date and time
+const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+document.getElementById('timeText').innerHTML = currentDate;
